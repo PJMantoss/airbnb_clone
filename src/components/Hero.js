89386211edfd1a932c06015ef1b5c939674009b6
PJMantoss <joelptoss@gmail.com>
2022-08-01 15:52:48 +0100

    update Hero styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        <img src='./photo-grid.png' alt='hero' />
-    </div>
+    <>
+        <div className='hero'>
+            <img 
+                src='./photo-grid.png' 
+                alt='hero' 
+            />
+        </div>
+        <div className='heroInfo'>
+            <h1>Online experiences</h1>
+            <p>Join unique interactive activities led
+                by one-of-a-kind host--all without
+                leaving home.
+            </p>
+        </div>
+    </>
   )
 }
 
