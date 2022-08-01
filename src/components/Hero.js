50a85fa551883749cd3f5181fb28b23d639411aa
PJMantoss <joelptoss@gmail.com>
2022-08-01 16:08:30 +0100

    update Hero styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
             src='./photo-grid.png' 
             alt='hero' 
         />
-        <h1>Online experiences</h1>
+        <h1>Online Experiences</h1>
         <p>Join unique interactive activities led
             by one-of-a-kind host--all without
             leaving home.
