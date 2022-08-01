8a198d99e094c2602d10892d7f5312bc1566264a
PJMantoss <joelptoss@gmail.com>
2022-08-01 18:43:03 +0100

    add Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div className='card'>
+        <img 
+            src='./katie-zaferes.png' 
+            alt='pic' 
+        />
+        <div className='cardInfo'>
+            <img 
+                src='./star.png' 
+                alt='pic' 
+            />
+            <span className='rating'>5.0</span>
+            <span className='desc'>(6) .</span>
+            <span className='price'>UK</span>
+        </div>
+        <p>Life lessons with Katie Zaferes</p>
+        <p>From $136 / person</p>
+    </div>
   )
 }
 
