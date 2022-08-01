b0f12046d5754b6c6d9fad9c9b211b4ce8c1c2b1
PJMantoss <joelptoss@gmail.com>
2022-08-01 15:32:34 +0100

    Add Hero component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <div className='wrapper'>
         <Navbar />
+        <Hero />
       </div>
     </div>
   );
