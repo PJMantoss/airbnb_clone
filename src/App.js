441f4a8be1259677f3fcbaa4ad57c91a7fc61435
PJMantoss <joelptoss@gmail.com>
2022-08-02 16:46:51 +0100

    add data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import Data from './data';
 import './App.css';
 
 function App() {
