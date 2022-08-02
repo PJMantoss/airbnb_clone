e318e67be671301fa9477d1ea031bee11b16826e
PJMantoss <joelptoss@gmail.com>
2022-08-02 20:32:35 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
 import Data from './data';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           />
         ))}
       </section>
+      <Footer />
     </div>
   );
 }
