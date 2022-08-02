8f2a6cf7a4e2d47cb05f31bd19299d53437c244b
PJMantoss <joelptoss@gmail.com>
2022-08-02 20:30:10 +0100

    add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>&copy; 2022 <strong>AirBnB Clone</strong></p>
+
+        <div id="love">
+            <span>Made With</span>
+            <img src="https://img.icons8.com/dusk/64/000000/like.png" alt="love"/> 
+            <span>by <a href="https://github.com/PJMantoss" target="_blank" rel="noreferrer">Mantoss</a></span>
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
